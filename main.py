cdac4a5bf1efa2d772bbc1243f3af16147e2ac83
hoop105ryouga <hoop105ryouga@icloud.com>
2022-07-18 01:24:10 +0900

    a

--- main.py
+++ main.py
@@ -28,6 +28,36 @@ import spreadsheet as myspred
  
 app = Flask(__name__)
 
+
+
+import sys
+# sys.path.append("/Users/hoop105ryouga/Documents/LineBot/.venv/lib/python3.9/site-packages")
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# use creds to create a client to interact with the Google Drive API
+scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# use creds to create a client to interact with the Google Drive API
+scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("飲食店DB").sheet1
+
+# Extract and print all of the values
+list_of_hashes = sheet.get_all_records()
+restaurant_info = list_of_hashes[0]
+info = ""
+for myvalue in restaurant_info.values():
+    info += myvalue
+
+
+
 #環境変数取得
 # LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
  
@@ -81,7 +111,7 @@ def handle_message(event):
 #   geo_info = f"{event.message.latitude} {event.message.longitude}"
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(myspred.info))#ここでメッセージを返します。
+        TextSendMessage(info))#ここでメッセージを返します。
 # def revert_json_py(s):
 #     d = json.loads(request.json)
 
