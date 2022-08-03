f16bb930731bbaa1a2bf0bd3a84b88f5e238d1d8
hoop105ryouga <hoop105ryouga@icloud.com>
2022-08-03 23:23:48 +0900

    a

--- spreadsheet.py
+++ spreadsheet.py
@@ -7,14 +7,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
-sheet = client.open("EngBot_Sheet1").sheet1
+sheet = client.open("LINE_UserId").sheet1
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
 user_id = "user_id"
     
 #空のcell(Noneが帰ってくるまで)に行き着くまでになるまで、繰り返し見ていく
-row_count = 1
-while sheet.cell(row_count, 1).value != None:
-    row_count += 1
-    print(sheet.cell(row_count, 1).value)
-list_of_hashes = sheet.update_cell(row_count, 1, user_id)
+# row_count = 1
+# while sheet.cell(row_count, 1).value != None:
+#     row_count += 1
+#     print(sheet.cell(row_count, 1).value)
+# list_of_hashes = sheet.update_cell(row_count, 1, user_id)
