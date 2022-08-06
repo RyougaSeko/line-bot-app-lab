3876048e97186da575f9f65f4f9add9da69c5ab7
hoop105ryouga <hoop105ryouga@icloud.com>
2022-08-07 01:46:56 +0900

    commit

--- createRichmenu.py
+++ createRichmenu.py
@@ -1,55 +1,55 @@
-# from linebot import (
-#     LineBotApi, WebhookHandler
-# )
+from linebot import (
+    LineBotApi, WebhookHandler
+)
 
-# from linebot.models import (
-#     MessageEvent, TextMessage, TextSendMessage, RichMenu, RichMenuSize, RichMenuArea, 
-#     RichMenuBounds, MessageAction, URIAction
-# )
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, RichMenu, RichMenuSize, RichMenuArea, 
+    RichMenuBounds, MessageAction, URIAction
+)
 
-# from lineapphandle import GenerateMessage
-# line_bot_api = LineBotApi('1BDCusHAfyLU9N+yl8EB1HQC4VFSgGs2AtLMQkwcg43qdf9STwQfONWPCM40W76h74Ad003w5ddcZdVSNoNcDH7h/opvM3UfoLasHEVRn1x13PrSx9kcGVz6w2SNxa02ne0VbNwZgf8Z0LLODSIK7AdB04t89/1O/w1cDnyilFU=')
+from lineapphandle import GenerateMessage
+line_bot_api = LineBotApi('1BDCusHAfyLU9N+yl8EB1HQC4VFSgGs2AtLMQkwcg43qdf9STwQfONWPCM40W76h74Ad003w5ddcZdVSNoNcDH7h/opvM3UfoLasHEVRn1x13PrSx9kcGVz6w2SNxa02ne0VbNwZgf8Z0LLODSIK7AdB04t89/1O/w1cDnyilFU=')
 
 
-# def createRichmenu():
-#     result = False
-#     try:
-#         # define a new richmenu
-#         rich_menu_to_create = RichMenu(
-#             size = RichMenuSize(width=1200, height=405),
-#             selected = True,
-#             name = 'richmenu for randomchat',
-#             chat_bar_text = 'TAP HERE',
-#             areas=[
+def createRichmenu():
+    result = False
+    try:
+        # define a new richmenu
+        rich_menu_to_create = RichMenu(
+            size = RichMenuSize(width=1200, height=405),
+            selected = True,
+            name = 'richmenu for randomchat',
+            chat_bar_text = 'TAP HERE',
+            areas=[
                 
-#                 RichMenuArea(
-#                     #1200x405を2分割
-#                     bounds=RichMenuBounds(x=0, y=0, width=600, height=405),
-#                     action=MessageAction(label = 'hello', text = 'わからない')
-#                 ),
-#                 RichMenuArea(
-#                     bounds=RichMenuBounds(x=600, y=0, width=600, height=405),
-#                     action=MessageAction(label = 'hello', text = 'わかる')
-#                 )
+                RichMenuArea(
+                    #1200x405を2分割
+                    bounds=RichMenuBounds(x=0, y=0, width=600, height=405),
+                    action=MessageAction(label = 'hello', text = 'わからない')
+                ),
+                # RichMenuArea(
+                #     bounds=RichMenuBounds(x=600, y=0, width=600, height=405),
+                #     action=MessageAction(label = 'hello', text = 'わかる')
+                # )
                 
-#             ]
-#         )
-#         richMenuId = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+            ]
+        )
+        richMenuId = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
 
-#         # upload an image for rich menu
-#         path = '../pic/know-or-dont.png'
+        # upload an image for rich menu
+        path = '../pic/know-or-dont.png'
 
-#         #ファイル読み込み
-#         with open(path, 'rb') as f:
-#             line_bot_api.set_rich_menu_image(richMenuId, "image/jpeg", f)
+        #ファイル読み込み
+        with open(path, 'rb') as f:
+            line_bot_api.set_rich_menu_image(richMenuId, "image/jpeg", f)
 
-#         # set the default rich menu
-#         line_bot_api.set_default_rich_menu(richMenuId)
+        # set the default rich menu
+        line_bot_api.set_default_rich_menu(richMenuId)
 
-#         result = True
+        result = True
 
-#     except Exception:
-#         result = False
+    except Exception:
+        result = False
 
 
-#     return result
+    return result
