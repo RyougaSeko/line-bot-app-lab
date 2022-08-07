362d0fc97273f95dd4fabc1cb3ad730af021d936
hoop105ryouga <hoop105ryouga@icloud.com>
2022-08-07 13:42:47 +0900

    a

--- main.py
+++ main.py
@@ -63,15 +63,14 @@ client = gspread.authorize(creds)
 line_bot_api = LineBotApi('1BDCusHAfyLU9N+yl8EB1HQC4VFSgGs2AtLMQkwcg43qdf9STwQfONWPCM40W76h74Ad003w5ddcZdVSNoNcDH7h/opvM3UfoLasHEVRn1x13PrSx9kcGVz6w2SNxa02ne0VbNwZgf8Z0LLODSIK7AdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('71b252f9a0355dc60dd372de730204bf')
 
-
-#王蟲返しapp
+createRichmenu()
 
 ## 1 ##
 #Webhookからのリクエストをチェックします。
 @app.route("/callback", methods=['POST'])
 def callback():
     #リッチメニューを呼び出す？
-    createRichmenu()
+    # createRichmenu()
     # リクエストヘッダーから署名検証のための値を取得します。
     signature = request.headers['X-Line-Signature']
  
